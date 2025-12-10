9e41290509616141f86bd9fac48e4d8a8f73307a
ahadsolo <kahad8192@gmail.com>
2025-12-10 11:36:31 +0530

    Create task 1.c

---
+++ task 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    // My Name
+    printf( "Name :Khan Ahad \n");
+    
+    // My age
+    printf("Age : 18 \n");
+    
+    // My Class
+    printf("Class: FYCE \n");
+
+    // My Division
+    printf("Division: C \n");
+    
+    // My UIN
+    printf("Uin: 251P003 \n");
+     
+    return 0;
+
+}
